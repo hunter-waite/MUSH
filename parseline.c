852b530c779ed77ea13168b427a991f0f1d83a21
ieatchickenz <abpetrov@gmail.com>
2019-11-18 15:11:54 -0800

    added printing format so we don't have to worry later

--- parseline.c
+++ parseline.c
@@ -14,3 +14,20 @@ void get_line(char *line,int size){
         exit(EXIT_FAILURE);
     }
 }
+
+void print_stage(const struct stage s){
+    int i;
+    
+    printf("--------\n");
+    printf("Stage %d: \"%s\"\n", s.snum, s.input);
+    printf("--------\n");
+    printf("%12s", "input: \0"); /*have to add actual input*/
+    printf("%12s", "output: \0"); /*add actual output*/
+    printf("%12s%d\n", "argc: \0", s.argcount);
+    printf("%12s", "argv: \0");
+    for(i = 0; i < s.argcount; i++){
+        printf("\"%s\", ", s.argv[i]);
+    }
+    printf("\n\n");
+ 
+}
--- parseline.h
+++ parseline.h
@@ -10,10 +10,12 @@
 
 typedef struct stage {
     int snum;
-    char input[LINESIZE];
-    int pipeto;
+    char input[LINESIZE]; /*make sure to store this with no newline*/
+    int pipeto;  /*could be 0, if so find output*/
     int argcount;
     char argv[NUMARGS][LINESIZE];
+    /*need some way to tell between inputs and outputs*/
 } stage;
 
 void get_line(char *line,int size);
+void print_stage(const struct stage);
