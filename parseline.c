975cce3ae4e3275b3a402ee5cd42a718190b44ac
hunter-waite <hwaite@calpoly.edu>
2019-11-18 11:31:41 -0800

    reads in the line when running parseline

--- parseline.c
+++ parseline.c
@@ -1,7 +1,16 @@
 #include "parseline.h"
 
 int main(int argc, char *argv[]){
-
-    
+    char line[LINESIZE];
+    get_line(line,LINESIZE);
+    printf(line);
     return 0;
+}
+
+void get_line(char *line,int size){
+    printf("line: ");
+    if(!fgets(line,size,stdin)){
+        perror("getline");
+        exit(EXIT_FAILURE);
+    }
 }
--- parseline.h
+++ parseline.h
@@ -1,4 +1,9 @@
 #include <string.h>
 #include <strings.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#define LINESIZE 512
+
+void get_line(char *line,int size);
